ab2cd9e0a2d64578ecda756be80437cbe0e9db6b
Lasry BESKIWIN <lasrybeskiwin@gmail.com>
2026-02-19 17:36:52 +0100

    Fonction lire_points()

--- voronoi.py
+++ voronoi.py
@@ -1,11 +1,14 @@
-fichier = "points.txt"
-points = []
-f = open(fichier, 'r')
+def lire_points(fichier):
+    points = []
+    f = open(fichier, 'r')
+    for ligne in f:
+        coordonnees = ligne.split(",")
+        x = float(coordonnees[0])
+        y = float(coordonnees[1])
+        points.append((x,y))
+    f.close()
+    return points
 
-for ligne in f:
-    coordonnees = ligne.split(",")
-    x = float(coordonnees[0])
-    y = float(coordonnees[1])
-    points.append((x,y))
+points = lire_points("points.txt")
 
 print(points)
