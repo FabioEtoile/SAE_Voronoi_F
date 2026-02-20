f64a07facf8745bbb5716dde28e69ec84198b240
Lasry BESKIWIN <lasrybeskiwin@gmail.com>
2026-02-20 18:40:19 +0100

    Création des vecteurs perpendiculaires à deux points et médiatrice

--- voronoi.py
+++ voronoi.py
@@ -22,6 +22,11 @@ points = lire_points("points.txt")
 largeur = 50
 hauteur = 50
 
+# Source https://cduck.github.io/drawsvg/
+d = dw.Drawing(largeur, hauteur)
+d.append(dw.Rectangle(0, 0, largeur, hauteur, fill="white"))
+
+
 mediatrice = []
 
 # (x1+x2)/2 , (y1+y2/2) = Coordonnées du point entre les deux points
@@ -34,15 +39,35 @@ for i in range(len(points)):
             xm = (point_actuel[0] + point_suivant[0]) / 2 
             ym = (point_actuel[1] + point_suivant[1]) / 2 
 
+            # Vecteur entre les deux points (AB = (xb-xa, yb-ya))
+            vx = point_suivant[0] - point_actuel[0]
+            vy = point_suivant[1] - point_actuel[1]
+
+            # https://www.reddit.com/r/learnmath/comments/1jjbe37/how_to_find_vectors_that_are_orthogonal_to_a/?tl=fr
+            # "Inverse les coordonnées et change le signe de l'une d'elles. Par exemple, [2,5] et [5,-2]"
+
+
+            vx_perpendiculaire = -vy
+            vy_perpendiculaire = vx
+
+
+            longueur = 50
+
+
+            x_debut = xm + vx_perpendiculaire * longueur
+            y_debut = ym + vy_perpendiculaire * longueur
+            x_fin = xm - vx_perpendiculaire * longueur
+            y_fin = ym - vy_perpendiculaire * longueur
+
+            d.append(dw.Line(x_debut, y_debut, x_fin, y_fin, stroke='blue'))
+
             mediatrice.append((xm,ym))
 
 print(mediatrice)
 
 # Création du SVG avec les points
 
-# Source https://cduck.github.io/drawsvg/
-d = dw.Drawing(largeur, hauteur)
-d.append(dw.Rectangle(0, 0, largeur, hauteur, fill="white"))
+
 rayon = 1
 
 for (x,y) in points:
