fe18a5b41d299942abe7d532976761e6c2f2bcfd
daviddiema7 <nozzytb@gmail.com>
2026-02-23 23:59:44 +0100

    Liaison du js avec le html et modification du js pour empecher le rechargement de la page quand aucun fichier n'est séléctionner

--- src/script.js
+++ src/script.js
@@ -5,21 +5,14 @@ let button_txt = document.getElementById("button-txt");
 let txt_cadre = document.getElementById("text-loading");
 
 function filesent() {
-    txt_cadre.textContent = "Génération de l'image ...";
+    let inputFichier = document.getElementById("point-txt");
 
-    //crée variable qui récupère le fichier
+    if (inputFichier.value == "") {
+        txt_cadre.textContent = "Aucun fichiers selectioner";
+    } else {
+        txt_cadre.textContent = "Génération de l'image ...";}
 
-    // lancer le programme python avec en paramètre le fichier
-
-    let img = "../points.png"
-    if (!img) {
-        console.log("Error, did not find image");
-        txt_cadre.textContent = "Erreur, image non généré";
-    }
-
-    txt_cadre.textContent = "";
-
-    // affiche image
+    
 }
 
 function downloadimage() {
